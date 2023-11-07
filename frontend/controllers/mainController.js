4eacf331262252b89205c83235cb262c84ac04f3
Matheus <matheus-13-@hotmail.com>
2023-11-07 20:42:33 -0300

    adding cors

--- frontend/controllers/mainController.js
+++ frontend/controllers/mainController.js
@@ -27,8 +27,28 @@ module.exports = class MainController {
             countrys.push(country);
         })
         
-        console.log(countrys)
+        //console.log(countrys)
         res.render("main/dashboard", {countrys});
+    }
+
+    static async postGraphList(req, res){
+        let listOfCountry = req.body["list"]
+        listOfCountry = {
+            "list" : listOfCountry
+        };
+
+        let listOfCountryJson = {};
+
+        await axios.post('http://127.0.0.1:8080/postGraph', listOfCountry)
+        .then(response => {
+            listOfCountryJson = response.data;
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+        console.log(listOfCountryJson)
+
     }
 
 }
--- frontend/routes/mainRoutes.js
+++ frontend/routes/mainRoutes.js
@@ -1,9 +1,10 @@
 const exppres = require('express');
 const router = exppres.Router();
 
-const MainController = require("../controllers/MainController");
+const MainController = require("../controllers/mainController");
 
 router.get("/", MainController.showHome);
 router.get("/dashboard", MainController.showDashboard);
+router.post("/postGraph", MainController.postGraphList);
 
 module.exports = router;
